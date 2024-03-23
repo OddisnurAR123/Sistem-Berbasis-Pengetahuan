a2d8a95ad396ece306547ded91c0f2ea0b460438
OddisnurAR123 <oddisnur@gmail.com>
2024-03-23 17:55:33 +0700

    QUIZ 1

--- QUIZ1/QuizNo7.java
+++ QUIZ1/QuizNo7.java
@@ -31,70 +31,70 @@ public class QuizNo7 {
             // R1
             if (A && B && !C) {
                 C = true;
-                System.out.println("Fakta baru C");
+                System.out.println("Fakta baru C bernilai benar");
                 adaPerubahan = true;
             }
 
             // R2
             if (C && !D) {
                 D = true;
-                System.out.println("Fakta baru D");
+                System.out.println("Fakta baru D bernilai benar");
                 adaPerubahan = true;
             }
 
             // R3
             if (A && E && !F) {
                 F = true;
-                System.out.println("Fakta baru F");
+                System.out.println("Fakta baru F bernilai benar");
                 adaPerubahan = true;
             }
 
             // R4
             if (A && !G) {
                 G = true;
-                System.out.println("Fakta baru G");
+                System.out.println("Fakta baru G bernilai benar");
                 adaPerubahan = true;
             }
 
             // R5
             if (F && G && !D) {
                 D = true;
-                System.out.println("Fakta baru D");
+                System.out.println("Fakta baru D bernilai benar");
                 adaPerubahan = true;
             }
 
             // R6
             if (G && E && !H) {
                 H = true;
-                System.out.println("Fakta baru H");
+                System.out.println("Fakta baru H bernilai benar");
                 adaPerubahan = true;
             }
 
             // R7
             if (C && H && !I) {
                 I = true;
-                System.out.println("Fakta baru I");
+                System.out.println("Fakta baru I bernilai benar");
                 adaPerubahan = true;
             }
 
             // R8
             if (I && A && !J) {
                 J = true;
-                System.out.println("Fakta baru J");
+                System.out.println("Fakta baru J bernilai benar");
                 adaPerubahan = true;
             }
 
             // R9
             if (G && !J) {
                 J = true;
-                System.out.println("Fakta baru J");
+                System.out.println("Fakta baru J bernilai benar");
                 adaPerubahan = true;
             }
 
             // R10
             if (J && !K) {
                 K = true;
-                System.out.println("Fakta baru K");
+                System.out.println("Fakta baru K bernilai benar");
                 adaPerubahan = true;
             }
         } while (adaPerubahan);
